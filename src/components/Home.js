1b2b1661d7acc2ac91bdfb4004067e71315888f7
Tiffany Man <tiffanyman19@yahoo.coom>
2024-01-29 10:26:00 -0500

    Complete Assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { Liza , Newyork } from "../data/user"
 
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        Liza is a Web Developer from New York
       </h1>
     </div>
   );
 }
+export default Home;
